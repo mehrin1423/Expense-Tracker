8e758195c5ef8e35f8197de7b849c982e3147922
mehrin1423 <118534211+mehrin1423@users.noreply.github.com>
2023-05-13 16:17:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,132 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0"
-    />
-    <title>Expense Tracker</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <h2>Expense Tracker</h2>
-    <div class="container">
-      <h4>Your Balance</h4>
-      <h1 id="balance">$0.00</h1>
-      <div class="inc-exp-container">
-        <div>
-          <h4>Income</h4>
-          <p id="money-plus" class="money-plus">
-            +$0.00
-          </p>
-        </div>
-        <div>
-          <h4>Expense</h4>
-          <p id="money-minus" class="money-minus">
-            -$0.00
-          </p>
-        </div>
-      </div>
-
-      <h3>History</h3>
-      <ul id="list" class="list">
-        <!-- <li class="minus">
-          Cash <span>-$400</span
-          ><button class="delete-btn">x</button>
-        </li> -->
-      </ul>
-      <h3>Add New Transition</h3>
-      <form id="form">
-          <div class="form-control">
-              <label for="text">Text</label>
-              <input type="text" id="text" placeholder="Enter Text...."/>
-          </div>
-          <div class="form-control">
-              <label for="amount">Amount <br> (negative - expense ,positive - income )</label>
-              <input type="number" id="amount" placeholder="Enter amount..."> 
-          </div>
-          <button class="btn">All transaction</button>
-      </form>
-    </div>
-    <!-- JavaYscript Project  -->
-    <script src="script.js"></script>
-  </body>
-</html>
+//1
+const balance = document.getElementById(
+    "balance"
+  );
+  const money_plus = document.getElementById(
+    "money-plus"
+  );
+  const money_minus = document.getElementById(
+    "money-minus"
+  );
+  const list = document.getElementById("list");
+  const form = document.getElementById("form");
+  const text = document.getElementById("text");
+  const amount = document.getElementById("amount");
+  // const dummyTransactions = [
+  //   { id: 1, text: "Flower", amount: -20 },
+  //   { id: 2, text: "Salary", amount: 300 },
+  //   { id: 3, text: "Book", amount: -10 },
+  //   { id: 4, text: "Camera", amount: 150 },
+  // ];
+  
+  // let transactions = dummyTransactions;
+  
+  //last 
+  const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
+  
+  let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+  
+  //5
+  //Add Transaction
+  function addTransaction(e){
+    e.preventDefault();
+    if(text.value.trim() === '' || amount.value.trim() === ''){
+      alert('please add text and amount')
+    }else{
+      const transaction = {
+        id:generateID(),
+        text:text.value,
+        amount:+amount.value
+      }
+  
+      transactions.push(transaction);
+  
+      addTransactionDOM(transaction);
+      updateValues();
+      updateLocalStorage();
+      text.value='';
+      amount.value='';
+    }
+  }
+  
+  
+  //5.5
+  //Generate Random ID
+  function generateID(){
+    return Math.floor(Math.random()*1000000000);
+  }
+  
+  //2
+  
+  //Add Trasactions to DOM list
+  function addTransactionDOM(transaction) {
+    //GET sign
+    const sign = transaction.amount < 0 ? "-" : "+";
+    const item = document.createElement("li");
+  
+    //Add Class Based on Value
+    item.classList.add(
+      transaction.amount < 0 ? "minus" : "plus"
+    );
+  
+    item.innerHTML = `
+      ${transaction.text} <span>${sign}${Math.abs(
+      transaction.amount
+    )}</span>
+      <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+      `;
+    list.appendChild(item);
+  }
+  
+  //4
+  
+  //Update the balance income and expence
+  function updateValues() {
+    const amounts = transactions.map(
+      (transaction) => transaction.amount
+    );
+    const total = amounts
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+    const income = amounts
+      .filter((item) => item > 0)
+      .reduce((acc, item) => (acc += item), 0)
+      .toFixed(2);
+    const expense =
+      (amounts
+        .filter((item) => item < 0)
+        .reduce((acc, item) => (acc += item), 0) *
+      -1).toFixed(2);
+  
+      balance.innerText=`$${total}`;
+      money_plus.innerText = `$${income}`;
+      money_minus.innerText = `$${expense}`;
+  }
+  
+  
+  //6 
+  
+  //Remove Transaction by ID
+  function removeTransaction(id){
+    transactions = transactions.filter(transaction => transaction.id !== id);
+    updateLocalStorage();
+    Init();
+  }
+  //last
+  //update Local Storage Transaction
+  function updateLocalStorage(){
+    localStorage.setItem('transactions',JSON.stringify(transactions));
+  }
+  
+  //3
+  
+  //Init App
+  function Init() {
+    list.innerHTML = "";
+    transactions.forEach(addTransactionDOM);
+    updateValues();
+  }
+  
+  Init();
+  
+  form.addEventListener('submit',addTransaction);
